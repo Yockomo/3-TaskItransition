5bb3790b5d4a7fe25eb2c3e203f3dc29308d1843
Yockomo <Funohs@yandex.ru>
2022-04-08 13:19:44 +0300

    Changed File Locations

---
+++ CsFiles/InputChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _3_Itransition
+{
+    static class InputChecker
+    {
+        public static void CheckInput(string[] args)
+        {
+            if (args.Length < 3)
+                ThrowException("Count of arguments < 3!");
+            else if (args.Length % 2 != 1)
+                ThrowException("Even numbers of arguments!");
+            else if (args.Distinct().Count() != args.Length)
+                ThrowException("One or few arguments are repeated!");
+        }
+        public static void ThrowException(string exceptionText)
+        {
+            throw new Exception($"{exceptionText} \n Correct input Example : rock scissors paper");
+        }
+    }
+}
